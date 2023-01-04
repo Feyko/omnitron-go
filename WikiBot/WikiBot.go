330bc8182c3c38086e291360a56afe7aee2a15fd
Anthony <lynkfox@gmail.com>
2023-01-04 16:00:08 -0500

    Refactored WikiBot to use QueryMapper interface for Get and a wrapper for getRecentChanges around that.

--- WikiBot/WikiBot.go
+++ WikiBot/WikiBot.go
@@ -3,10 +3,13 @@ package WikiBot
 import (
 	"github.com/pkg/errors"
 
+	mwApi "github.com/lynkfox/omnitron-go/WikiBot/mediaWikiApi"
+
 	"cgt.name/pkg/go-mwclient"
 	"github.com/antonholmquist/jason"
 )
 
+// Core contains the central parts of this WikiBot connections.
 type Core struct {
 	Username string
 	Password string
@@ -14,7 +17,7 @@ type Core struct {
 	client   mwclient.Client
 }
 
-/* Connect generates a client and logs in, storing the client on the Core struct
+/* Connect generates a client and logs in, storing the client on the Core struct.
  */
 func (bot *Core) Connect() error {
 	client, err := mwclient.New(bot.WikiUrl, bot.Username)
@@ -34,21 +37,28 @@ func (bot *Core) Connect() error {
 }
 
 /*
-GetRecentChanges queries the Recent Changes List from the api and returns a
-list of the changes, or nil and an error if it fails.
+WikiGet sends a GET request to the wiki client logged in.
 */
-func (bot *Core) GetRecentChanges() (*jason.Object, error) {
-	parameters := map[string]string{
-		"action":   "query",
-		"list":     "recentchanges",
-		"rclimit":  "2",
-		"rctype":   "edit",
-		"continue": "",
+func (bot *Core) WikiGet(parameters mwApi.QueryMapper) (*jason.Object, error) {
+	resp, err := bot.client.Get(parameters.Map())
+	if err != nil {
+		return nil, errors.Wrap(err, "GetFailed")
+	}
+	return resp, nil
+}
+
+/*
+GetRecentChanges is a wrapper around WikiGet for a default or custom RecentChanges query.
+*/
+func (bot *Core) GetRecentChanges(parameters *mwApi.RecentChanges) (*jason.Object, error) {
+	if parameters == nil {
+		var defaultParam mwApi.RecentChanges
+		parameters = &defaultParam
 	}
 
-	resp, err := bot.client.Get(parameters)
+	resp, err := bot.WikiGet(parameters)
 	if err != nil {
-		return nil, errors.Wrap(err, "Recent Changes failed to get")
+		return nil, errors.Wrap(err, "RecentChanges: %v")
 	}
 
 	return resp, nil
